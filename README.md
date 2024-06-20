f3e793f1d7afd2a1cc2421f43d9bfe1dda1962ae
Andrii Sotnyk <sotnyk.external@crxmarkets.com>
2024-06-21 01:54:27 +0200

    Add an integration test for the REST API

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 Application to compute optimal distribution of available rooms in the hotel (economy and premium) between guests looking for a room.
 
-Run HotelOccupancyManager.class and open in browser http://localhost:8008/occupancy?premium=3&economy=3
+Run HotelOccupancyManager.class and open in browser http://localhost:8008/occupancy?premium=3&economy=3, providing the number of free premium and economy rooms.
+Port can be changed in application.properties file.
+
+Two test classes are present:
+OccupancyServiceTest.class - testing different business scenarios (combinations of rooms and prices)
+OccupancyControllerIT.class - simple integration test for the REST API
+
+The prices guests are ready to pay are hardcoded when running an app normally, but can be changed in the tests.
+
---
+++ src/test/java/sotnyk/andrii/controller/OccupancyControllerIT.java
@@ -0,0 +1,47 @@
+package sotnyk.andrii.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import sotnyk.andrii.HotelOccupancyManager;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = HotelOccupancyManager.class)
+@AutoConfigureMockMvc
+public class OccupancyControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHappyPath() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/occupancy")
+                        .queryParam("premium", "3")
+                        .queryParam("economy", "3")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .json("{\"ECONOMY\":{\"rooms\":3,\"rent\":167.99},\"PREMIUM\":{\"rooms\":3,\"rent\":738.0}}"));
+    }
+
+    @Test
+    public void testWrongParams() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/occupancy")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MissingServletRequestParameterException));
+    }
+}
